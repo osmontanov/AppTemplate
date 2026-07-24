95269747bd63202f102066d15403c4cb7bb52a33
talgar osmonov <88522587+osmontanov@users.noreply.github.com>
2026-07-24 11:36:06 +0600

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by aurora on 24/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
