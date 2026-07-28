aae13339a917f0152818f5e8f085e288ec064622
talgar osmonov <88522587+osmontanov@users.noreply.github.com>
2026-07-28 18:28:49 +0600

    refactor: centralize application models

---
+++ AppTemplate/App/Models/Domain/NavigationGuideItem.swift
@@ -0,0 +1,8 @@
+nonisolated struct NavigationGuideItem:
+    Identifiable,
+    Equatable,
+    Sendable {
+    let id: String
+    let title: String
+    let systemImage: String
+}
--- AppTemplate/Features/Home/ViewModels/NavigationGuideViewModel.swift
+++ AppTemplate/Features/Home/ViewModels/NavigationGuideViewModel.swift
@@ -1,11 +1,5 @@
 import Observation
 
-nonisolated struct NavigationGuideItem: Identifiable, Equatable, Sendable {
-    let id: String
-    let title: String
-    let systemImage: String
-}
-
 @MainActor
 @Observable
 final class NavigationGuideViewModel {
